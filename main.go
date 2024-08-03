e35d5298cd253ce4bdaa8ab2ecb2a5c9e02f857c
nothing-c <halskdjf@gmail.com>
2024-08-03 15:56:07 -0500

    Opening zip file working

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
     "archive/zip"
+    "fmt"
 )
 
 func main () {
     T:="Impreza Wiring.docx"
-
+    R,e:=zip.OpenReader(T)
+    if e!=nil { panic(e) } 
+    for _,f := range R.File { fmt.Println(f.Name) }
+    defer R.Close()
 }
