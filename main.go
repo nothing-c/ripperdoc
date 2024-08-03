cd778f86403f8d96ac74e4430f5b4bde7a023a3b
nothing-c <halskdjf@gmail.com>
2024-08-03 15:58:08 -0500

    Edit README formatting

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main () {
     T:="Impreza Wiring.docx"
     R,e:=zip.OpenReader(T)
     if e!=nil { panic(e) } 
-    for _,f := range R.File { fmt.Println(f.Name) }
-    defer R.Close()
+    
+    R.Close()
 }
