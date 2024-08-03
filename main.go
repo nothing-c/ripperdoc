381f2ea728560382493a37d82fd5179fa3969930
nothing-c <halskdjf@gmail.com>
2024-08-03 16:29:30 -0500

    Put dumping into own function

--- main.go
+++ main.go
@@ -3,21 +3,18 @@ package main
 import (
     "archive/zip"
     "fmt"
-    //"encoding/xml"
     "regexp"
     "io"
+//    "flag"
 )
 
-func main () {
-    T:="Impreza Wiring.docx"
+func dump (T string) {
     R,e:=zip.OpenReader(T)
     if e!=nil { panic(e) } 
     for _,f := range R.File { 
         if f.Name=="word/document.xml" { r,e:=f.Open(); if e!=nil { panic(e) }
-            //x:=xml.NewDecoder(r)
             re,e:=regexp.Compile(`<w:t.*?>(.*?)<\/w:t>`); if e!=nil { panic(e) }
             x,e:=io.ReadAll(r); if e!=nil { panic(e) }
-            //fmt.Printf("%q \n", re.FindAllStringSubmatch(string(x),-1))
             for _,m := range re.FindAllStringSubmatch(string(x),-1) {
                 fmt.Println(m[1])
             }
@@ -25,4 +22,9 @@ func main () {
         }
     }
     R.Close()
+}
+
+func main () {
+    //T:="Impreza Wiring.docx"
+    dump("Impreza Wiring.docx")
 }
