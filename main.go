7a8fe2d630c0ac1cd94d915c879c8e78df37b124
nothing-c <halskdjf@gmail.com>
2024-08-03 16:19:51 -0500

    Dumping text only to stdout

--- main.go
+++ main.go
@@ -17,10 +17,12 @@ func main () {
             //x:=xml.NewDecoder(r)
             re,e:=regexp.Compile(`<w:t>(.*?)<\/w:t>`); if e!=nil { panic(e) }
             x,e:=io.ReadAll(r); if e!=nil { panic(e) }
-            fmt.Printf("%q    \n", re.FindAll(x,-1))
+            //fmt.Printf("%q \n", re.FindAllStringSubmatch(string(x),-1))
+            for _,m := range re.FindAllStringSubmatch(string(x),-1) {
+                fmt.Println(m[1])
+            }
             r.Close(); break
         }
-        fmt.Println("No document.xml found!")
     }
     R.Close()
 }
