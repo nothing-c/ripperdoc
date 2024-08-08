a91b91f57ab09671456b6baba3994f0899f5384f
nothing-c <halskdjf@gmail.com>
2024-08-08 17:18:17 -0500

    Parallelized

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
     "io"
     "flag"
     "os"
+    "sync"
 )
 
-func dump (T string) {
+func dump (T string, C chan string) {
+    var S string
     R,e:=zip.OpenReader(T)
     if e!=nil { panic(e) } 
     for _,f := range R.File { 
@@ -17,12 +19,14 @@ func dump (T string) {
             re,e:=regexp.Compile(`<w:t.*?>(.*?)<\/w:t>`); if e!=nil { panic(e) }
             x,e:=io.ReadAll(r); if e!=nil { panic(e) }
             for _,m := range re.FindAllStringSubmatch(string(x),-1) {
-                fmt.Println(m[1])
+                S += m[1] + "\n"
+                //fmt.Println(m[1])
             }
             r.Close(); break
         }
     }
     R.Close()
+    C <- S
 }
 
 func help () {
@@ -36,14 +40,27 @@ func help () {
 }
 
 func main () {
-    Oo:=flag.String("o","rdout","Write output to a file instead of stdout")
+    var OF *os.File
+    Oo:=flag.String("o","","Write output to a file instead of stdout")
     Os:=flag.Bool("s",false,"Enable 'smart mode', which tries to reconstruct the docx file's structure")
     Oh:=flag.Bool("h",false,"Display this message")
     flag.Parse()
+    C:=make(chan string,flag.NArg()) // Enough to buffer all the output 
+    var W sync.WaitGroup
+    var e error // Have to do this to make the compiler happy -__-
+    if *Oo != "" { OF,e = os.OpenFile(*Oo, os.O_RDWR|os.O_CREATE, 0644); if e!=nil { panic(e) } } else { OF = os.Stdout }
     if *Oh == true { help() }
-    if *Oo != "rdout" { fmt.Println("output file: " + *Oo) }
     if *Os != false { fmt.Println("smart mode on") }
     for _,f := range flag.Args() {
-        dump(f)
+        W.Add(1)
+        go func(s string) {
+            defer W.Done()
+            dump(s,C)
+        }(f)
     }
+    W.Wait()
+    for len(C) > 0 {
+        _,e := io.WriteString(OF,<-C + "\n~\n"); if e!=nil { panic(e) }
+    }
+    os.Exit(0)
 }
